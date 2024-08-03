80ae827959746e1d609374214494e420249ad39e
Anime King <177407363+Infinity-Anime-King@users.noreply.github.com>
2024-08-03 20:02:11 +0530

    Create Type_B.py

---
+++ Type_B.py
@@ -0,0 +1,11 @@
+n = 10 #The length
+t = 5 #The width
+for i in range(t): #the times it wants to print (uses from 't')
+    print("* " * (n)) # prints the string multiple times (uses 'n' to multipley)
+
+# Res;
+# * * * * * * * * * * 
+# * * * * * * * * * *
+# * * * * * * * * * *
+# * * * * * * * * * *
+# * * * * * * * * * *
